cowboyox/mapbox

commit 013bd4aa1416f64685776f9159f6baa74001f1d2

fix bug where styles might not get unset

js/style/style.js
@@ -116,7 +116,7 @@ Style.prototype.cascade = function() {
         }
     }
 
-    var layers = this.layers;
+    var layers = {};
 
     // Calculate new transitions
     for (name in newStyle) {
@@ -127,7 +127,7 @@ Style.prototype.cascade = function() {
         for (prop in layer) {
             var newDeclaration = new StyleDeclaration(prop, layer[prop], this.stylesheet.constants);
 
-            var oldTransition = layers[name][prop];
+            var oldTransition = this.layers[name] && this.layers[name][prop];
             var transition = transitions[name][prop];
 
             // Only create a new transition if the declaration changed
@@ -141,6 +141,8 @@ Style.prototype.cascade = function() {
             }
         }
     }
+
+    this.layers = layers;
 
     this.recalculate();
     this.fire('change');

commit 3410f93c01192c40db0ce7fada8c8581a74bee42

retain old transitions when there's no new one
fixes bug introduced by 013bd4aa1416f64685776f9159f6baa74001f1d2

js/style/style.js
@@ -138,6 +138,8 @@ Style.prototype.cascade = function() {
                 // Run the animation loop until the end of the transition
                 newTransition.loopID = this.animationLoop.set(newTransition.endTime - (new Date()).getTime());
                 if (oldTransition) this.animationLoop.cancel(oldTransition.loopID);
+            } else {
+                layers[name][prop] = oldTransition;
             }
         }
     }