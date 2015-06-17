mapbox/mapbox-gl-js

Fontstack as arrays
* Refs https://github.com/mapbox/mapbox-gl-style-spec/pull/258
* Wildly non-backward-compatible
* Relies on next release of mapbox-gl-style-spec

js/data/symbol_bucket.js
@@ -67,7 +67,7 @@ SymbolBucket.prototype.addFeatures = function() {
     var lineHeight = layoutProperties['text-line-height'] * oneEm;
     var maxWidth = layoutProperties['symbol-placement'] !== 'line' && layoutProperties['text-max-width'] * oneEm;
     var spacing = layoutProperties['text-letter-spacing'] * oneEm;
-    var fontstack = layoutProperties['text-font'];
+    var fontstack = layoutProperties['text-font'].join(',');
     var textOffset = [layoutProperties['text-offset'][0] * oneEm, layoutProperties['text-offset'][1] * oneEm];
 
     var geometries = [],

cowboyox/mapbox

js/data/symbol_bucket.js
@@ -80,7 +80,7 @@ SymbolBucket.prototype.addFeatures = function(collisionTile) {
     var maxWidth = layout['symbol-placement'] !== 'line' ? layout['text-max-width'] * oneEm : 0;
     var spacing = layout['text-letter-spacing'] * oneEm;
     var textOffset = [layout['text-offset'][0] * oneEm, layout['text-offset'][1] * oneEm];
-    var fontstack = layout['text-font'];
+    var fontstack = layout['text-font'].join(',');
 
     var geometries = [];
     for (var g = 0; g < features.length; g++) {