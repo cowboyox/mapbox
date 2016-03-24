maphubs/mapbox-gl-js

move queryRenderedFeatures to the main thread
- it's fast
- this helps avoid worker postMessage clone costs
- this prevents busy workers from delaying the results
- this is a step towards lazily constructing GeoJSON and eliminating
  `includeGeometry`

js/source/worker_tile.js
@@ -21,7 +21,7 @@ function WorkerTile(params) {
     this.collisionDebug = params.collisionDebug;
 }
 
-WorkerTile.prototype.parse = function(data, layers, actor, callback) {
+WorkerTile.prototype.parse = function(data, layers, actor, callback, rawTileData) {
 
     this.status = 'parsing';
     this.data = data;
@@ -203,11 +203,15 @@ WorkerTile.prototype.parse = function(data, layers, actor, callback) {
             tile.redoPlacementAfterDone = false;
         }
 
+        var featureTree = tile.featureTree.serialize();
+        featureTree.data.rawTileData = rawTileData;
+
         callback(null, {
             elementGroups: getElementGroups(buckets),
             buffers: buffers,
-            bucketStats: stats
-        }, getTransferables(buffers));
+            bucketStats: stats,
+            featureTree: featureTree.data
+        }, getTransferables(buffers).concat(featureTree.transferables.concat(rawTileData)));
     }
 };
 

cowboyox/mapbox

js/source/worker_tile.js
@@ -21,7 +21,7 @@ function WorkerTile(params) {
     this.showCollisionBoxes = params.showCollisionBoxes;
 }
 
-WorkerTile.prototype.parse = function(data, layers, actor, callback) {
+WorkerTile.prototype.parse = function(data, layers, actor, callback, rawTileData) {
 
     this.status = 'parsing';
     this.data = data;
@@ -202,10 +202,14 @@ WorkerTile.prototype.parse = function(data, layers, actor, callback) {
             tile.redoPlacementAfterDone = false;
         }
 
+        var featureTree = tile.featureTree.serialize();
+        featureTree.data.rawTileData = rawTileData;
+
         callback(null, {
             buckets: buckets.filter(isBucketEmpty).map(serializeBucket),
-            bucketStats: stats // TODO put this in a separate message?
-        }, getTransferables(buckets));
+            bucketStats: stats, // TODO put this in a separate message?
+            featureTree: featureTree.data
+        }, getTransferables(buckets).concat(featureTree.transferables.concat(rawTileData)));
     }
 };
 