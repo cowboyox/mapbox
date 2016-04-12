maphubs/mapbox-gl-js

Add buffer offsets to element groups

js/render/draw_circle.js
@@ -54,16 +54,13 @@ function drawCircles(painter, source, layer, coords) {
 
         for (var k = 0; k < elementGroups.length; k++) {
             var group = elementGroups[k];
-            var offset = group.vertexStartIndex * vertex.itemSize;
+            var count = group.elementLength * 3;
 
             vertex.bind(gl);
-            bucket.setAttribPointers('circle', gl, shader, offset, [{$zoom: painter.transform.zoom}]);
-
             elements.bind(gl);
 
-            var count = group.elementLength * 3;
-            var elementOffset = group.elementStartIndex * elements.itemSize;
-            gl.drawElements(gl.TRIANGLES, count, gl.UNSIGNED_SHORT, elementOffset);
+            bucket.setAttribPointers('circle', gl, shader, group.vertexOffset, [{$zoom: painter.transform.zoom}]);
+            gl.drawElements(gl.TRIANGLES, count, gl.UNSIGNED_SHORT, group.elementOffset);
         }
 
         bucket.unsetAttribPointers('circle', gl, shader);

js/render/draw_collision_debug.js
@@ -21,7 +21,7 @@ function drawCollisionDebug(painter, source, layer, coords) {
 
         var buffer = bucket.buffers.collisionBoxVertex;
         buffer.bind(gl);
-        bucket.setAttribPointers('collisionBox', gl, shader, 0);
+        bucket.setAttribPointers('collisionBox', gl, shader, elementGroups[0].vertexOffset);
 
         var posMatrix = painter.calculatePosMatrix(coord, source.maxzoom);
         gl.setPosMatrix(posMatrix);

js/render/draw_line.js
@@ -174,12 +174,10 @@ module.exports = function drawLine(painter, source, layer, coords) {
 
         for (var i = 0; i < elementGroups.length; i++) {
             var group = elementGroups[i];
-            var vtxOffset = group.vertexStartIndex * vertex.itemSize;
-            bucket.setAttribPointers('line', gl, shader, vtxOffset);
+            bucket.setAttribPointers('line', gl, shader, group.vertexOffset);
 
             var count = group.elementLength * 3;
-            var elementOffset = group.elementStartIndex * element.itemSize;
-            gl.drawElements(gl.TRIANGLES, count, gl.UNSIGNED_SHORT, elementOffset);
+            gl.drawElements(gl.TRIANGLES, count, gl.UNSIGNED_SHORT, group.elementOffset);
         }
     }
 

js/render/draw_symbol.js
@@ -152,7 +152,7 @@ function drawSymbol(painter, layer, posMatrix, tile, bucket, elementGroups, pref
     gl.uniform1f(shader.u_maxfadezoom, Math.floor(f.maxfadezoom * 10));
     gl.uniform1f(shader.u_fadezoom, (painter.transform.zoom + f.bump) * 10);
 
-    var group, offset, count, elementOffset;
+    var group, count;
 
     elements.bind(gl);
 
@@ -172,13 +172,11 @@ function drawSymbol(painter, layer, posMatrix, tile, bucket, elementGroups, pref
 
             for (var j = 0; j < elementGroups.length; j++) {
                 group = elementGroups[j];
-                offset = group.vertexStartIndex * vertex.itemSize;
                 vertex.bind(gl);
-                bucket.setAttribPointers(shaderInterfaceName, gl, shader, offset);
+                bucket.setAttribPointers(shaderInterfaceName, gl, shader, group.vertexOffset);
 
                 count = group.elementLength * 3;
-                elementOffset = group.elementStartIndex * elements.itemSize;
-                gl.drawElements(gl.TRIANGLES, count, gl.UNSIGNED_SHORT, elementOffset);
+                gl.drawElements(gl.TRIANGLES, count, gl.UNSIGNED_SHORT, group.elementOffset);
             }
         }
 
@@ -189,26 +187,22 @@ function drawSymbol(painter, layer, posMatrix, tile, bucket, elementGroups, pref
 
         for (var i = 0; i < elementGroups.length; i++) {
             group = elementGroups[i];
-            offset = group.vertexStartIndex * vertex.itemSize;
             vertex.bind(gl);
-            bucket.setAttribPointers(shaderInterfaceName, gl, shader, offset);
+            bucket.setAttribPointers(shaderInterfaceName, gl, shader, group.vertexOffset);
 
             count = group.elementLength * 3;
-            elementOffset = group.elementStartIndex * elements.itemSize;
-            gl.drawElements(gl.TRIANGLES, count, gl.UNSIGNED_SHORT, elementOffset);
+            gl.drawElements(gl.TRIANGLES, count, gl.UNSIGNED_SHORT, group.elementOffset);
         }
 
     } else {
         gl.uniform1f(shader.u_opacity, layer.paint['icon-opacity']);
         for (var k = 0; k < elementGroups.length; k++) {
             group = elementGroups[k];
-            offset = group.vertexStartIndex * vertex.itemSize;
             vertex.bind(gl);
-            bucket.setAttribPointers(shaderInterfaceName, gl, shader, offset);
+            bucket.setAttribPointers(shaderInterfaceName, gl, shader, group.vertexOffset);
 
             count = group.elementLength * 3;
-            elementOffset = group.elementStartIndex * elements.itemSize;
-            gl.drawElements(gl.TRIANGLES, count, gl.UNSIGNED_SHORT, elementOffset);
+            gl.drawElements(gl.TRIANGLES, count, gl.UNSIGNED_SHORT, group.elementOffset);
         }
     }
 }

cowboyox/mapbox

js/render/draw_circle.js
@@ -50,16 +50,13 @@ function drawCircles(painter, source, layer, coords) {
 
         for (var k = 0; k < elementGroups.length; k++) {
             var group = elementGroups[k];
-            var offset = group.vertexStartIndex * vertex.itemSize;
+            var count = group.elementLength * 3;
 
             vertex.bind(gl);
-            bucket.setAttribPointers('circle', gl, program, offset, [{$zoom: painter.transform.zoom}]);
-
             elements.bind(gl);
 
-            var count = group.elementLength * 3;
-            var elementOffset = group.elementStartIndex * elements.itemSize;
-            gl.drawElements(gl.TRIANGLES, count, gl.UNSIGNED_SHORT, elementOffset);
+            bucket.setAttribPointers('circle', gl, program, group.vertexOffset, [{$zoom: painter.transform.zoom}]);
+            gl.drawElements(gl.TRIANGLES, count, gl.UNSIGNED_SHORT, group.elementOffset);
         }
     }
 

js/render/draw_collision_debug.js
@@ -20,7 +20,7 @@ function drawCollisionDebug(painter, source, layer, coords) {
 
         var buffer = bucket.buffers.collisionBoxVertex;
         buffer.bind(gl);
-        bucket.setAttribPointers('collisionBox', gl, program, 0);
+        bucket.setAttribPointers('collisionBox', gl, program, elementGroups[0].vertexOffset);
 
         painter.setPosMatrix(coord.posMatrix);
 

js/render/draw_line.js
@@ -168,13 +168,10 @@ module.exports = function drawLine(painter, source, layer, coords) {
 
         for (var i = 0; i < elementGroups.length; i++) {
             var group = elementGroups[i];
-
-            var vtxOffset = group.vertexStartIndex * vertex.itemSize;
-            bucket.setAttribPointers('line', gl, program, vtxOffset);
+            bucket.setAttribPointers('line', gl, program, group.vertexOffset);
 
             var count = group.elementLength * 3;
-            var elementOffset = group.elementStartIndex * element.itemSize;
-            gl.drawElements(gl.TRIANGLES, count, gl.UNSIGNED_SHORT, elementOffset);
+            gl.drawElements(gl.TRIANGLES, count, gl.UNSIGNED_SHORT, group.elementOffset);
         }
     }
 

js/render/draw_symbol.js
@@ -146,7 +146,7 @@ function drawSymbol(painter, layer, posMatrix, tile, bucket, elementGroups, pref
     painter.frameHistory.bind(gl);
     gl.uniform1i(program.u_fadetexture, 1);
 
-    var group, offset, count, elementOffset;
+    var group, count;
 
     elements.bind(gl);
 
@@ -166,13 +166,11 @@ function drawSymbol(painter, layer, posMatrix, tile, bucket, elementGroups, pref
 
             for (var j = 0; j < elementGroups.length; j++) {
                 group = elementGroups[j];
-                offset = group.vertexStartIndex * vertex.itemSize;
                 vertex.bind(gl);
-                bucket.setAttribPointers(programInterfaceName, gl, program, offset);
+                bucket.setAttribPointers(programInterfaceName, gl, program, group.vertexOffset);
 
                 count = group.elementLength * 3;
-                elementOffset = group.elementStartIndex * elements.itemSize;
-                gl.drawElements(gl.TRIANGLES, count, gl.UNSIGNED_SHORT, elementOffset);
+                gl.drawElements(gl.TRIANGLES, count, gl.UNSIGNED_SHORT, group.elementOffset);
             }
         }
 
@@ -183,26 +181,22 @@ function drawSymbol(painter, layer, posMatrix, tile, bucket, elementGroups, pref
 
         for (var i = 0; i < elementGroups.length; i++) {
             group = elementGroups[i];
-            offset = group.vertexStartIndex * vertex.itemSize;
             vertex.bind(gl);
-            bucket.setAttribPointers(programInterfaceName, gl, program, offset);
+            bucket.setAttribPointers(programInterfaceName, gl, program, group.vertexOffset);
 
             count = group.elementLength * 3;
-            elementOffset = group.elementStartIndex * elements.itemSize;
-            gl.drawElements(gl.TRIANGLES, count, gl.UNSIGNED_SHORT, elementOffset);
+            gl.drawElements(gl.TRIANGLES, count, gl.UNSIGNED_SHORT, group.elementOffset);
         }
 
     } else {
         gl.uniform1f(program.u_opacity, layer.paint['icon-opacity']);
         for (var k = 0; k < elementGroups.length; k++) {
             group = elementGroups[k];
-            offset = group.vertexStartIndex * vertex.itemSize;
             vertex.bind(gl);
-            bucket.setAttribPointers(programInterfaceName, gl, program, offset);
+            bucket.setAttribPointers(programInterfaceName, gl, program, group.vertexOffset);
 
             count = group.elementLength * 3;
-            elementOffset = group.elementStartIndex * elements.itemSize;
-            gl.drawElements(gl.TRIANGLES, count, gl.UNSIGNED_SHORT, elementOffset);
+            gl.drawElements(gl.TRIANGLES, count, gl.UNSIGNED_SHORT, group.elementOffset);
         }
     }
 }