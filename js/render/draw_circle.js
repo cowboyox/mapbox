thtr/mapbox-gl-js

Allow attributes to be disabled

js/render/draw_circle.js
@@ -39,6 +39,7 @@ function drawCircles(painter, source, layer, coords) {
         var elementGroups = tile.getElementGroups(layer, 'circle');
         if (!elementGroups) continue;
 
+        var bucket = tile.buckets[layer.ref || layer.id];
         var vertex = tile.buffers.circleVertex;
         var elements = tile.buffers.circleElement;
 
@@ -55,7 +56,7 @@ function drawCircles(painter, source, layer, coords) {
             var offset = group.vertexStartIndex * vertex.itemSize;
 
             vertex.bind(gl);
-            vertex.setAttribPointers(gl, shader, offset);
+            bucket.setAttribPointers('circle', gl, shader, offset, [{$zoom: painter.transform.zoom}]);
 
             elements.bind(gl);
 
@@ -68,5 +69,6 @@ function drawCircles(painter, source, layer, coords) {
     gl.enable(gl.STENCIL_TEST);
     gl.enableVertexAttribArray(shader.a_blur);
     gl.enableVertexAttribArray(shader.a_size);
+    bucket.unsetAttribPointers('circle', gl, shader);
 
 }

cowboyox/mapbox

js/render/draw_circle.js
@@ -33,6 +33,7 @@ function drawCircles(painter, source, layer, coords) {
         var tile = source.getTile(coord);
         var bucket = tile.getBucket(layer);
         if (!bucket) continue;
+        bucket.createStyleLayer(layer);
         var elementGroups = bucket.elementGroups.circle;
         if (!elementGroups) continue;
 
@@ -52,7 +53,7 @@ function drawCircles(painter, source, layer, coords) {
             var offset = group.vertexStartIndex * vertex.itemSize;
 
             vertex.bind(gl);
-            vertex.setAttribPointers(gl, program, offset);
+            bucket.setAttribPointers('circle', gl, program, offset, [{$zoom: painter.transform.zoom}]);
 
             elements.bind(gl);
 