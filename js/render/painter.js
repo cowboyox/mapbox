vincentsarago/mapbox-gl-js

add an overdraw inspector
The overdraw inspector shows how many time each fragment has been
shaded. White fragments were shaded 8 or more times. Black fragments
were shaded 0 times. Lower is better.

`map.showOverdrawInspector = true` turns it on.

js/render/painter.js
@@ -64,7 +64,6 @@ Painter.prototype.resize = function(width, height) {
 
 };
 
-
 Painter.prototype.setup = function() {
     var gl = this.gl;
 
@@ -225,6 +224,8 @@ Painter.prototype.render = function(style, options) {
     this.clearColor();
     this.clearDepth();
 
+    this.overdrawInspector(options.overdrawInspector);
+
     this.depthRange = (style._order.length + 2) * this.numSublayers * this.depthEpsilon;
 
     this.renderPass({isOpaquePass: true});
@@ -255,7 +256,9 @@ Painter.prototype.renderPass = function(options) {
         }
 
         if (isOpaquePass) {
-            this.gl.disable(this.gl.BLEND);
+            if (!this._overdrawInspector) {
+                this.gl.disable(this.gl.BLEND);
+            }
             this.isOpaquePass = true;
         } else {
             this.gl.enable(this.gl.BLEND);
@@ -373,4 +376,21 @@ Painter.prototype.setExMatrix = function(exMatrix) {
 
 Painter.prototype.lineWidth = function(width) {
     this.gl.lineWidth(util.clamp(width, this.lineWidthRange[0], this.lineWidthRange[1]));
+};
+
+Painter.prototype.overdrawInspector = function(enabled) {
+    if (!enabled && !this._overdrawInspector) return;
+    this._overdrawInspector = enabled;
+
+    var gl = this.gl;
+    if (enabled) {
+        gl.blendFunc(gl.CONSTANT_COLOR, gl.ONE);
+        var numOverdrawSteps = 8;
+        var a = 1 / numOverdrawSteps;
+        gl.blendColor(a, a, a, 0);
+        gl.clearColor(0, 0, 0, 1);
+        gl.clear(gl.COLOR_BUFFER_BIT);
+    } else {
+        gl.blendFunc(gl.ONE, gl.ONE_MINUS_SRC_ALPHA);
+    }
 };

js/ui/map.js
@@ -847,6 +847,7 @@ util.extend(Map.prototype, /** @lends Map.prototype */{
 
         this.painter.render(this.style, {
             debug: this.showTileBoundaries,
+            overdrawInspector: this._showOverdrawInspector,
             vertices: this.vertices,
             rotating: this.rotating,
             zooming: this.zooming
@@ -995,6 +996,21 @@ util.extendAll(Map.prototype, /** @lends Map.prototype */{
         if (this._showCollisionBoxes === value) return;
         this._showCollisionBoxes = value;
         this.style._redoPlacement();
+    },
+
+    /*
+     * Show how many times each fragment has been shaded. White fragments have
+     * been shaded 8 or more times. Black fragments have been shaded 0 times.
+     *
+     * @name showOverdraw
+     * @type {boolean}
+     */
+    _showOverdrawInspector: false,
+    get showOverdrawInspector() { return this._showOverdrawInspector; },
+    set showOverdrawInspector(value) {
+        if (this._showOverdrawInspector === value) return;
+        this._showOverdrawInspector = value;
+        this._update();
     },
 
     /**

cowboyox/mapbox

js/render/painter.js
@@ -64,7 +64,6 @@ Painter.prototype.resize = function(width, height) {
 
 };
 
-
 Painter.prototype.setup = function() {
     var gl = this.gl;
 
@@ -225,6 +224,8 @@ Painter.prototype.render = function(style, options) {
     this.clearColor();
     this.clearDepth();
 
+    this.showOverdrawInspector(options.showOverdrawInspector);
+
     this.depthRange = (style._order.length + 2) * this.numSublayers * this.depthEpsilon;
 
     this.renderPass({isOpaquePass: true});
@@ -255,7 +256,9 @@ Painter.prototype.renderPass = function(options) {
         }
 
         if (isOpaquePass) {
-            this.gl.disable(this.gl.BLEND);
+            if (!this._showOverdrawInspector) {
+                this.gl.disable(this.gl.BLEND);
+            }
             this.isOpaquePass = true;
         } else {
             this.gl.enable(this.gl.BLEND);
@@ -373,4 +376,21 @@ Painter.prototype.setExMatrix = function(exMatrix) {
 
 Painter.prototype.lineWidth = function(width) {
     this.gl.lineWidth(util.clamp(width, this.lineWidthRange[0], this.lineWidthRange[1]));
+};
+
+Painter.prototype.showOverdrawInspector = function(enabled) {
+    if (!enabled && !this._showOverdrawInspector) return;
+    this._showOverdrawInspector = enabled;
+
+    var gl = this.gl;
+    if (enabled) {
+        gl.blendFunc(gl.CONSTANT_COLOR, gl.ONE);
+        var numOverdrawSteps = 8;
+        var a = 1 / numOverdrawSteps;
+        gl.blendColor(a, a, a, 0);
+        gl.clearColor(0, 0, 0, 1);
+        gl.clear(gl.COLOR_BUFFER_BIT);
+    } else {
+        gl.blendFunc(gl.ONE, gl.ONE_MINUS_SRC_ALPHA);
+    }
 };

js/ui/map.js
@@ -877,6 +877,7 @@ util.extend(Map.prototype, /** @lends Map.prototype */{
 
         this.painter.render(this.style, {
             debug: this.showTileBoundaries,
+            showOverdrawInspector: this._showOverdrawInspector,
             vertices: this.vertices,
             rotating: this.rotating,
             zooming: this.zooming
@@ -1025,6 +1026,21 @@ util.extendAll(Map.prototype, /** @lends Map.prototype */{
         if (this._showCollisionBoxes === value) return;
         this._showCollisionBoxes = value;
         this.style._redoPlacement();
+    },
+
+    /*
+     * Show how many times each fragment has been shaded. White fragments have
+     * been shaded 8 or more times. Black fragments have been shaded 0 times.
+     *
+     * @name showOverdraw
+     * @type {boolean}
+     */
+    _showOverdrawInspector: false,
+    get showOverdrawInspector() { return this._showOverdrawInspector; },
+    set showOverdrawInspector(value) {
+        if (this._showOverdrawInspector === value) return;
+        this._showOverdrawInspector = value;
+        this._update();
     },
 
     /**