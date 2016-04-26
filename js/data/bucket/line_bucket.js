vincentsarago/mapbox-gl-js

replace elementGroups with bufferGroups
create separate arrays/buffers for each group
- it's rare to have multiple buffers so the overhead is low
- each array already tracks it's own length
- this way you don't need to store the offset at the beginning

js/data/bucket/line_bucket.js
@@ -50,8 +50,8 @@ function LineBucket() {
 
 LineBucket.prototype = util.inherit(Bucket, {});
 
-LineBucket.prototype.addLineVertex = function(point, extrude, tx, ty, dir, linesofar) {
-    return this.arrays.line.layout.vertex.emplaceBack(
+LineBucket.prototype.addLineVertex = function(vertexBuffer, point, extrude, tx, ty, dir, linesofar) {
+    return vertexBuffer.emplaceBack(
             // a_pos
             (point.x << 1) | tx,
             (point.y << 1) | ty,
@@ -368,25 +368,24 @@ LineBucket.prototype.addLine = function(vertices, join, cap, miterLimit, roundLi
 LineBucket.prototype.addCurrentVertex = function(currentVertex, distance, normal, endLeft, endRight, round) {
     var tx = round ? 1 : 0;
     var extrude;
-    var group = this.elementGroups.line[this.elementGroups.line.length - 1];
-    group.vertexLength += 2;
+    var layoutArrays = this.arrays.line[this.arrays.line.length - 1].layout;
+    var vertexArray = layoutArrays.vertex;
+    var elementArray = layoutArrays.element;
 
     extrude = normal.clone();
     if (endLeft) extrude._sub(normal.perp()._mult(endLeft));
-    this.e3 = this.addLineVertex(currentVertex, extrude, tx, 0, endLeft, distance) - group.vertexStartIndex;
+    this.e3 = this.addLineVertex(vertexArray, currentVertex, extrude, tx, 0, endLeft, distance);
     if (this.e1 >= 0 && this.e2 >= 0) {
-        this.arrays.line.layout.element.emplaceBack(this.e1, this.e2, this.e3);
-        group.elementLength++;
+        elementArray.emplaceBack(this.e1, this.e2, this.e3);
     }
     this.e1 = this.e2;
     this.e2 = this.e3;
 
     extrude = normal.mult(-1);
     if (endRight) extrude._sub(normal.perp()._mult(endRight));
-    this.e3 = this.addLineVertex(currentVertex, extrude, tx, 1, -endRight, distance) - group.vertexStartIndex;
+    this.e3 = this.addLineVertex(vertexArray, currentVertex, extrude, tx, 1, -endRight, distance);
     if (this.e1 >= 0 && this.e2 >= 0) {
-        this.arrays.line.layout.element.emplaceBack(this.e1, this.e2, this.e3);
-        group.elementLength++;
+        elementArray.emplaceBack(this.e1, this.e2, this.e3);
     }
     this.e1 = this.e2;
     this.e2 = this.e3;
@@ -414,14 +413,14 @@ LineBucket.prototype.addCurrentVertex = function(currentVertex, distance, normal
 LineBucket.prototype.addPieSliceVertex = function(currentVertex, distance, extrude, lineTurnsLeft) {
     var ty = lineTurnsLeft ? 1 : 0;
     extrude = extrude.mult(lineTurnsLeft ? -1 : 1);
-    var group = this.elementGroups.line[this.elementGroups.line.length - 1];
+    var layoutArrays = this.arrays.line[this.arrays.line.length - 1].layout;
+    var vertexArray = layoutArrays.vertex;
+    var elementArray = layoutArrays.element;
 
-    this.e3 = this.addLineVertex(currentVertex, extrude, 0, ty, 0, distance) - group.vertexStartIndex;
-    group.vertexLength++;
+    this.e3 = this.addLineVertex(vertexArray, currentVertex, extrude, 0, ty, 0, distance);
 
     if (this.e1 >= 0 && this.e2 >= 0) {
-        this.arrays.line.layout.element.emplaceBack(this.e1, this.e2, this.e3);
-        group.elementLength++;
+        elementArray.emplaceBack(this.e1, this.e2, this.e3);
     }
 
     if (lineTurnsLeft) {

js/render/draw_circle.js
@@ -22,8 +22,8 @@ function drawCircles(painter, source, layer, coords) {
         var tile = source.getTile(coord);
         var bucket = tile.getBucket(layer);
         if (!bucket) continue;
-        var elementGroups = bucket.elementGroups.circle;
-        if (!elementGroups) continue;
+        var bufferGroups = bucket.buffers.circle;
+        if (!bufferGroups) continue;
 
         var program = painter.useProgram('circle', bucket.getProgramMacros('circle', layer));
 
@@ -41,16 +41,10 @@ function drawCircles(painter, source, layer, coords) {
 
         bucket.setUniforms(gl, 'circle', program, layer, {zoom: painter.transform.zoom});
 
-        var buffers = bucket.buffers.circle;
-        var vertexBuffer = buffers.layout.vertex;
-        var elementBuffer = buffers.layout.element;
-        var paintVertexBuffer = buffers.paint[layer.id];
-
-        for (var k = 0; k < elementGroups.length; k++) {
-            var group = elementGroups[k];
-            var count = group.elementLength * 3;
-            group.vaos[layer.id].bind(gl, program, vertexBuffer, paintVertexBuffer, group.vertexStartIndex, elementBuffer);
-            gl.drawElements(gl.TRIANGLES, count, gl.UNSIGNED_SHORT, group.elementOffset);
+        for (var k = 0; k < bufferGroups.length; k++) {
+            var group = bufferGroups[k];
+            group.vaos[layer.id].bind(gl, program, group.layout.vertex, group.layout.element, group.paint[layer.id]);
+            gl.drawElements(gl.TRIANGLES, group.layout.element.length * 3, gl.UNSIGNED_SHORT, 0);
         }
     }
 }

js/render/draw_fill.js
@@ -74,8 +74,8 @@ function drawFill(painter, source, layer, coord) {
     var tile = source.getTile(coord);
     var bucket = tile.getBucket(layer);
     if (!bucket) return;
-    var elementGroups = bucket.elementGroups.fill;
-    if (!elementGroups) return;
+    var bufferGroups = bucket.buffers.fill;
+    if (!bufferGroups) return;
 
     var gl = painter.gl;
 
@@ -115,15 +115,10 @@ function drawFill(painter, source, layer, coord) {
     var fillProgram = painter.useProgram('fill');
     gl.uniformMatrix4fv(fillProgram.u_matrix, false, translatedPosMatrix);
 
-    var buffers = bucket.buffers.fill.layout;
-    var vertexBuffer = buffers.vertex;
-    var elementBuffer = buffers.element;
-
-    for (var i = 0; i < elementGroups.length; i++) {
-        var group = elementGroups[i];
-        var count = group.elementLength * 3;
-        group.vaos[layer.id].bind(gl, fillProgram, vertexBuffer, undefined, group.vertexStartIndex, elementBuffer);
-        gl.drawElements(gl.TRIANGLES, count, gl.UNSIGNED_SHORT, group.elementOffset);
+    for (var i = 0; i < bufferGroups.length; i++) {
+        var group = bufferGroups[i];
+        group.vaos[layer.id].bind(gl, fillProgram, group.layout.vertex, group.layout.element);
+        gl.drawElements(gl.TRIANGLES, group.layout.element.length * 3, gl.UNSIGNED_SHORT, 0);
     }
 
     // Now that we have the stencil mask in the stencil buffer, we can start
@@ -144,14 +139,14 @@ function drawFill(painter, source, layer, coord) {
         gl.activeTexture(gl.TEXTURE0);
         painter.spriteAtlas.bind(gl, true);
 
-        painter.tileExtentPatternVAO.bind(gl, program, painter.tileExtentBuffer, undefined, 0, undefined);
+        painter.tileExtentPatternVAO.bind(gl, program, painter.tileExtentBuffer);
 
     } else {
         // Draw filling rectangle.
         program = painter.useProgram('fill');
         gl.uniform4fv(fillProgram.u_color, color);
         gl.uniform1f(fillProgram.u_opacity, opacity);
-        painter.tileExtentVAO.bind(gl, program, painter.tileExtentBuffer, undefined, 0, undefined);
+        painter.tileExtentVAO.bind(gl, program, painter.tileExtentBuffer);
     }
 
     gl.uniformMatrix4fv(program.u_matrix, false, posMatrix);
@@ -170,7 +165,7 @@ function drawStroke(painter, source, layer, coord) {
     if (!bucket) return;
 
     var gl = painter.gl;
-    var elementGroups = bucket.elementGroups.fill;
+    var bufferGroups = bucket.buffers.fill;
 
     var image = layer.paint['fill-pattern'];
     var opacity = layer.paint['fill-opacity'];
@@ -185,17 +180,12 @@ function drawStroke(painter, source, layer, coord) {
 
     if (image) { setPattern(image, opacity, tile, coord, painter, program); }
 
-    var buffers = bucket.buffers.fill.layout;
-    var vertexBuffer = buffers.vertex;
-    var elementBuffer = buffers.secondElement;
-
     painter.enableTileClippingMask(coord);
 
-    for (var k = 0; k < elementGroups.length; k++) {
-        var group = elementGroups[k];
-        var count = group.secondElementLength * 2;
-        group.secondVaos[layer.id].bind(gl, program, vertexBuffer, undefined, group.vertexStartIndex, elementBuffer);
-        gl.drawElements(gl.LINES, count, gl.UNSIGNED_SHORT, group.secondElementOffset);
+    for (var k = 0; k < bufferGroups.length; k++) {
+        var group = bufferGroups[k];
+        group.secondVaos[layer.id].bind(gl, program, group.layout.vertex, group.layout.secondElement);
+        gl.drawElements(gl.LINES, group.layout.secondElement.length * 2, gl.UNSIGNED_SHORT, 0);
     }
 }
 

cowboyox/mapbox

js/data/bucket/line_bucket.js
@@ -50,8 +50,8 @@ function LineBucket() {
 
 LineBucket.prototype = util.inherit(Bucket, {});
 
-LineBucket.prototype.addLineVertex = function(point, extrude, tx, ty, dir, linesofar) {
-    return this.arrays.line.layout.vertex.emplaceBack(
+LineBucket.prototype.addLineVertex = function(vertexBuffer, point, extrude, tx, ty, dir, linesofar) {
+    return vertexBuffer.emplaceBack(
             // a_pos
             (point.x << 1) | tx,
             (point.y << 1) | ty,
@@ -368,25 +368,24 @@ LineBucket.prototype.addLine = function(vertices, join, cap, miterLimit, roundLi
 LineBucket.prototype.addCurrentVertex = function(currentVertex, distance, normal, endLeft, endRight, round) {
     var tx = round ? 1 : 0;
     var extrude;
-    var group = this.elementGroups.line[this.elementGroups.line.length - 1];
-    group.vertexLength += 2;
+    var layoutArrays = this.arrayGroups.line[this.arrayGroups.line.length - 1].layout;
+    var vertexArray = layoutArrays.vertex;
+    var elementArray = layoutArrays.element;
 
     extrude = normal.clone();
     if (endLeft) extrude._sub(normal.perp()._mult(endLeft));
-    this.e3 = this.addLineVertex(currentVertex, extrude, tx, 0, endLeft, distance) - group.vertexStartIndex;
+    this.e3 = this.addLineVertex(vertexArray, currentVertex, extrude, tx, 0, endLeft, distance);
     if (this.e1 >= 0 && this.e2 >= 0) {
-        this.arrays.line.layout.element.emplaceBack(this.e1, this.e2, this.e3);
-        group.elementLength++;
+        elementArray.emplaceBack(this.e1, this.e2, this.e3);
     }
     this.e1 = this.e2;
     this.e2 = this.e3;
 
     extrude = normal.mult(-1);
     if (endRight) extrude._sub(normal.perp()._mult(endRight));
-    this.e3 = this.addLineVertex(currentVertex, extrude, tx, 1, -endRight, distance) - group.vertexStartIndex;
+    this.e3 = this.addLineVertex(vertexArray, currentVertex, extrude, tx, 1, -endRight, distance);
     if (this.e1 >= 0 && this.e2 >= 0) {
-        this.arrays.line.layout.element.emplaceBack(this.e1, this.e2, this.e3);
-        group.elementLength++;
+        elementArray.emplaceBack(this.e1, this.e2, this.e3);
     }
     this.e1 = this.e2;
     this.e2 = this.e3;
@@ -414,14 +413,14 @@ LineBucket.prototype.addCurrentVertex = function(currentVertex, distance, normal
 LineBucket.prototype.addPieSliceVertex = function(currentVertex, distance, extrude, lineTurnsLeft) {
     var ty = lineTurnsLeft ? 1 : 0;
     extrude = extrude.mult(lineTurnsLeft ? -1 : 1);
-    var group = this.elementGroups.line[this.elementGroups.line.length - 1];
+    var layoutArrays = this.arrayGroups.line[this.arrayGroups.line.length - 1].layout;
+    var vertexArray = layoutArrays.vertex;
+    var elementArray = layoutArrays.element;
 
-    this.e3 = this.addLineVertex(currentVertex, extrude, 0, ty, 0, distance) - group.vertexStartIndex;
-    group.vertexLength++;
+    this.e3 = this.addLineVertex(vertexArray, currentVertex, extrude, 0, ty, 0, distance);
 
     if (this.e1 >= 0 && this.e2 >= 0) {
-        this.arrays.line.layout.element.emplaceBack(this.e1, this.e2, this.e3);
-        group.elementLength++;
+        elementArray.emplaceBack(this.e1, this.e2, this.e3);
     }
 
     if (lineTurnsLeft) {

js/render/draw_circle.js
@@ -22,8 +22,8 @@ function drawCircles(painter, source, layer, coords) {
         var tile = source.getTile(coord);
         var bucket = tile.getBucket(layer);
         if (!bucket) continue;
-        var elementGroups = bucket.elementGroups.circle;
-        if (!elementGroups) continue;
+        var bufferGroups = bucket.bufferGroups.circle;
+        if (!bufferGroups) continue;
 
         var program = painter.useProgram('circle', bucket.getProgramMacros('circle', layer));
 
@@ -41,16 +41,10 @@ function drawCircles(painter, source, layer, coords) {
 
         bucket.setUniforms(gl, 'circle', program, layer, {zoom: painter.transform.zoom});
 
-        var buffers = bucket.buffers.circle;
-        var vertexBuffer = buffers.layout.vertex;
-        var elementBuffer = buffers.layout.element;
-        var paintVertexBuffer = buffers.paint[layer.id];
-
-        for (var k = 0; k < elementGroups.length; k++) {
-            var group = elementGroups[k];
-            var count = group.elementLength * 3;
-            group.vaos[layer.id].bind(gl, program, vertexBuffer, paintVertexBuffer, group.vertexStartIndex, elementBuffer);
-            gl.drawElements(gl.TRIANGLES, count, gl.UNSIGNED_SHORT, group.elementOffset);
+        for (var k = 0; k < bufferGroups.length; k++) {
+            var group = bufferGroups[k];
+            group.vaos[layer.id].bind(gl, program, group.layout.vertex, group.layout.element, group.paint[layer.id]);
+            gl.drawElements(gl.TRIANGLES, group.layout.element.length * 3, gl.UNSIGNED_SHORT, 0);
         }
     }
 }

js/render/draw_fill.js
@@ -74,8 +74,8 @@ function drawFill(painter, source, layer, coord) {
     var tile = source.getTile(coord);
     var bucket = tile.getBucket(layer);
     if (!bucket) return;
-    var elementGroups = bucket.elementGroups.fill;
-    if (!elementGroups) return;
+    var bufferGroups = bucket.bufferGroups.fill;
+    if (!bufferGroups) return;
 
     var gl = painter.gl;
 
@@ -115,15 +115,10 @@ function drawFill(painter, source, layer, coord) {
     var fillProgram = painter.useProgram('fill');
     gl.uniformMatrix4fv(fillProgram.u_matrix, false, translatedPosMatrix);
 
-    var buffers = bucket.buffers.fill.layout;
-    var vertexBuffer = buffers.vertex;
-    var elementBuffer = buffers.element;
-
-    for (var i = 0; i < elementGroups.length; i++) {
-        var group = elementGroups[i];
-        var count = group.elementLength * 3;
-        group.vaos[layer.id].bind(gl, fillProgram, vertexBuffer, undefined, group.vertexStartIndex, elementBuffer);
-        gl.drawElements(gl.TRIANGLES, count, gl.UNSIGNED_SHORT, group.elementOffset);
+    for (var i = 0; i < bufferGroups.length; i++) {
+        var group = bufferGroups[i];
+        group.vaos[layer.id].bind(gl, fillProgram, group.layout.vertex, group.layout.element);
+        gl.drawElements(gl.TRIANGLES, group.layout.element.length * 3, gl.UNSIGNED_SHORT, 0);
     }
 
     // Now that we have the stencil mask in the stencil buffer, we can start
@@ -144,14 +139,14 @@ function drawFill(painter, source, layer, coord) {
         gl.activeTexture(gl.TEXTURE0);
         painter.spriteAtlas.bind(gl, true);
 
-        painter.tileExtentPatternVAO.bind(gl, program, painter.tileExtentBuffer, undefined, 0, undefined);
+        painter.tileExtentPatternVAO.bind(gl, program, painter.tileExtentBuffer);
 
     } else {
         // Draw filling rectangle.
         program = painter.useProgram('fill');
         gl.uniform4fv(fillProgram.u_color, color);
         gl.uniform1f(fillProgram.u_opacity, opacity);
-        painter.tileExtentVAO.bind(gl, program, painter.tileExtentBuffer, undefined, 0, undefined);
+        painter.tileExtentVAO.bind(gl, program, painter.tileExtentBuffer);
     }
 
     gl.uniformMatrix4fv(program.u_matrix, false, posMatrix);
@@ -170,7 +165,7 @@ function drawStroke(painter, source, layer, coord) {
     if (!bucket) return;
 
     var gl = painter.gl;
-    var elementGroups = bucket.elementGroups.fill;
+    var bufferGroups = bucket.bufferGroups.fill;
 
     var image = layer.paint['fill-pattern'];
     var opacity = layer.paint['fill-opacity'];
@@ -185,17 +180,12 @@ function drawStroke(painter, source, layer, coord) {
 
     if (image) { setPattern(image, opacity, tile, coord, painter, program); }
 
-    var buffers = bucket.buffers.fill.layout;
-    var vertexBuffer = buffers.vertex;
-    var elementBuffer = buffers.secondElement;
-
     painter.enableTileClippingMask(coord);
 
-    for (var k = 0; k < elementGroups.length; k++) {
-        var group = elementGroups[k];
-        var count = group.secondElementLength * 2;
-        group.secondVaos[layer.id].bind(gl, program, vertexBuffer, undefined, group.vertexStartIndex, elementBuffer);
-        gl.drawElements(gl.LINES, count, gl.UNSIGNED_SHORT, group.secondElementOffset);
+    for (var k = 0; k < bufferGroups.length; k++) {
+        var group = bufferGroups[k];
+        group.secondVaos[layer.id].bind(gl, program, group.layout.vertex, group.layout.element2);
+        gl.drawElements(gl.LINES, group.layout.element2.length * 2, gl.UNSIGNED_SHORT, 0);
     }
 }
 