jiebai/mapbox-gl-js

Make evented#addEventListener idempotent

test/unit/util/evented.test.js
@@ -119,6 +119,17 @@ test('Evented', (t) => {
         t.end();
     });
 
+    t.test('on is idempotent', (t) => {
+      const evented = new Evented();
+      const listener = t.spy();
+      evented.on('a', listener);
+      evented.on('a', listener);
+      evented.on('a', listener);
+      evented.fire(new Event('a'));
+      t.ok(listener.calledOnce);
+      t.end();
+    });
+
     t.test('evented parents', (t) => {
 
         t.test('adds parents with "setEventedParent"', (t) => {

cowboyox/mapbox

test/unit/util/evented.test.js
@@ -119,6 +119,19 @@ test('Evented', (t) => {
         t.end();
     });
 
+    t.test('on is idempotent', (t) => {
+        const evented = new Evented();
+        const listenerA = t.spy();
+        const listenerB = t.spy();
+        evented.on('a', listenerA);
+        evented.on('a', listenerB);
+        evented.on('a', listenerA);
+        evented.fire(new Event('a'));
+        t.ok(listenerA.calledOnce);
+        t.ok(listenerA.calledBefore(listenerB));
+        t.end();
+    });
+
     t.test('evented parents', (t) => {
 
         t.test('adds parents with "setEventedParent"', (t) => {